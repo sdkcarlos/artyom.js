98c189678fe06f14e6ec4919076a16a71499081a
Carlos Delgado <sdkcarlos@outlook.com>
2015-10-08 08:09:17 -0500

    Fix Commands V 0.6

--- src/artyomCommands.js
+++ src/artyomCommands.js
@@ -8,7 +8,7 @@
  * @param {type} window
  * @Description Artyom JS Core - See Documentation for further information
  * @see http://sdkcarlos.github.io/artyom.html
- * @ignore 27.07.2015 17:17
+ * @ignore 08.10.2015 17:17
  * @returns {object}
  */
 
@@ -38,46 +38,5 @@
         //Continue adding your own commands here
     ];
     
-    
-    /**
-     * Artyom Commands Functions
-     * 
-     * @returns {artyomCommands_L13.ArtyomCommands.artyCommands}
-     */
-    function ArtyomCommands(){
-        var artyCommands = {}; 
-        
-        /**
-         * This functions is called in the artyom.js file
-         * is necessary to load this file when you use artyom.
-         * 
-         * @param {type} lang
-         * @returns {Array}
-         */
-        artyCommands.getCommands = function(lang){
-            return artyomCommands;
-        };
-        
-        
-        /**
-         * Create explicits commands in other views where this commands is not
-         * needed all the time
-         * 
-         * @param {type} commands
-         * @returns {Array}
-         */
-        artyCommands.extend = function(commands){
-            commands.forEach(function(comando) {
-                artyomCommands.push(comando);
-            });
-            
-            return artyomCommands;
-        };
-        
-        return artyCommands;
-    }
-    
-    if(typeof(artyCommands) === 'undefined'){
-        window.artyCommands = ArtyomCommands();
-    }
+    artyom.addCommands(artyomCommands);
 })(window);
